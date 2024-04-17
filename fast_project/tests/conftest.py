f4061d46dc781e775279953d2d293ed3e185b0f2
mugdhakolte <koltemugdha@gmail.com>
2024-04-17 17:49:51 +0200

    Added pytest configuration and a test

---
+++ fast_project/tests/conftest.py
@@ -0,0 +1,18 @@
+import os
+
+import pytest
+from starlette.testclient import TestClient
+
+from fast_app import main
+from fast_app.config import get_settings, Settings
+
+
+def get_settings_override():
+    return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    main.app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(main.app) as test_client:
+        yield test_client

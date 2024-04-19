6a94f453c850ae68c1f94718d9f22f88cb1933d6
mugdhakolte <koltemugdha@gmail.com>
2024-04-19 16:48:59 +0200

    Refactored tests of Update rest API

--- fast_project/tests/test_summaries.py
+++ fast_project/tests/test_summaries.py
@@ -1,5 +1,7 @@
 import json
 
+import pytest
+
 
 def test_create_summary(test_app_with_db):
     response = test_app_with_db.post(
@@ -140,93 +142,80 @@ def test_update_summary(test_app_with_db):
     assert response_dict["created_at"]
 
 
-def test_update_summary_incorrect_id(test_app_with_db):
+@pytest.mark.parametrize(
+    "summary_id, payload, status_code, detail",
+    [
+        [
+            999,
+            {"url": "https://koltemugdha.wordpress.com/", "summary": "updated!"},
+            404,
+            "Summary not found",
+        ],
+        [
+            0,
+            {"url": "https://koltemugdha.wordpress.com/", "summary": "updated!"},
+            422,
+            [
+                {
+                    "type": "greater_than",
+                    "loc": ["path", "id"],
+                    "msg": "Input should be greater than 0",
+                    "input": "0",
+                    "ctx": {"gt": 0},
+                    "url": "https://errors.pydantic.dev/2.7/v/greater_than",
+                }
+            ],
+        ],
+        [
+            1,
+            {},
+            422,
+            [
+                {
+                    "type": "missing",
+                    "loc": ["body", "url"],
+                    "msg": "Field required",
+                    "input": {},
+                    "url": "https://errors.pydantic.dev/2.7/v/missing",
+                },
+                {
+                    "type": "missing",
+                    "loc": ["body", "summary"],
+                    "msg": "Field required",
+                    "input": {},
+                    "url": "https://errors.pydantic.dev/2.7/v/missing",
+                },
+            ],
+        ],
+        [
+            1,
+            {"url": "https://koltemugdha.wordpress.com/"},
+            422,
+            [
+                {
+                    "type": "missing",
+                    "loc": ["body", "summary"],
+                    "msg": "Field required",
+                    "input": {"url": "https://koltemugdha.wordpress.com/"},
+                    "url": "https://errors.pydantic.dev/2.7/v/missing",
+                }
+            ],
+        ],
+    ],
+)
+def test_update_summary_invalid(
+    test_app_with_db, summary_id, payload, status_code, detail
+):
     response = test_app_with_db.put(
-        "/summaries/999/",
-        data=json.dumps(
-            {"url": "https://koltemugdha.wordpress.com/", "summary": "updated!"}
-        ),
+        f"/summaries/{summary_id}/", data=json.dumps(payload)
     )
-    assert response.status_code == 404
-    assert response.json()["detail"] == "Summary not found"
-
-    response = test_app_with_db.put(
-        "/summaries/0/",
-        data=json.dumps(
-            {"url": "https://koltemugdha.wordpress.com/", "summary": "updated!"}
-        ),
-    )
-    assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "ctx": {"gt": 0},
-                "input": "0",
-                "loc": ["path", "id"],
-                "msg": "Input should be greater than 0",
-                "type": "greater_than",
-                "url": "https://errors.pydantic.dev/2.7/v/greater_than",
-            }
-        ]
-    }
+    assert response.status_code == status_code
+    assert response.json()["detail"] == detail
 
 
-def test_update_summary_invalid_json(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries", data=json.dumps({"url": "https://koltemugdha.wordpress.com/"})
-    )
-    summary_id = response.json()["id"]
-
-    response = test_app_with_db.put(
-        f"/summaries/{summary_id}/",
-        data=json.dumps({}),
-    )
-    assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "input": {},
-                "loc": ["body", "url"],
-                "msg": "Field required",
-                "type": "missing",
-                "url": "https://errors.pydantic.dev/2.7/v/missing",
-            },
-            {
-                "input": {},
-                "loc": ["body", "summary"],
-                "msg": "Field required",
-                "type": "missing",
-                "url": "https://errors.pydantic.dev/2.7/v/missing",
-            },
-        ]
-    }
-
-
-def test_update_summary_invalid_keys(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://koltemugdha.wordpress.com/"})
-    )
-    summary_id = response.json()["id"]
-
-    response = test_app_with_db.put(
-        f"/summaries/{summary_id}/",
-        data=json.dumps({"url": "https://koltemugdha.wordpress.com/"}),
-    )
-    assert response.status_code == 422
-    assert response.json() == {
-        "detail": [
-            {
-                "input": {"url": "https://koltemugdha.wordpress.com/"},
-                "loc": ["body", "summary"],
-                "msg": "Field required",
-                "type": "missing",
-                "url": "https://errors.pydantic.dev/2.7/v/missing",
-            }
-        ]
-    }
-
-    response = test_app_with_db.put(
-        f"/summaries/{summary_id}/",
+def test_update_summary_invalid_url(test_app):
+    response = test_app.put(
+        "/summaries/1/",
         data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
     )
     assert response.status_code == 422

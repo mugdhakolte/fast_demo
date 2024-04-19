2facfd5795d8291cbb97accc862e01322c5bca73
mugdhakolte <koltemugdha@gmail.com>
2024-04-19 15:20:15 +0200

    Added Delete Rest API and tests

--- fast_project/fast_app/api/summaries.py
+++ fast_project/fast_app/api/summaries.py
@@ -28,3 +28,14 @@ async def read_summary(id: int) -> SummarySchema:
 @router.get("/", response_model=List[SummarySchema])
 async def read_all_summaries() -> List[SummarySchema]:
     return await crud.get_all()
+
+
+@router.delete("/{id}/", response_model=SummaryResponseSchema)
+async def delete_summary(id: int) -> SummaryResponseSchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    await crud.delete(id)
+
+    return summary
--- fast_project/tests/test_summaries.py
+++ fast_project/tests/test_summaries.py
@@ -61,3 +61,23 @@ def test_read_all_summaries(test_app_with_db):
 
     response_list = response.json()
     assert len(list(filter(lambda d: d["id"] == summary_id, response_list))) == 1
+
+
+def test_remove_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries", data=json.dumps({"url": "https://koltemugdha.wordpress.com/"})
+    )
+    summary_id = response.json()["id"]
+
+    response = test_app_with_db.delete(f"summaries/{summary_id}/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": summary_id,
+        "url": "https://koltemugdha.wordpress.com/",
+    }
+
+
+def test_remove_summary_incorrect_id(test_app_with_db):
+    response = test_app_with_db.delete("/summaries/999/")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Summary not found"
